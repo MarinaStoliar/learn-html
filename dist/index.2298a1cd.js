fc4d742ec7370d6a9e8ad2c8f8cdefee89d41754
Oleg Grabets <oleg.hrabets@infotech.gov.ua>
2023-04-30 21:50:15 +0300

    remove dist folder

--- dist/index.2298a1cd.js
+++
@@ -1,4 +0,0 @@
-const s = "Start test. (TS support)";
-console.log(s);
-
-//# sourceMappingURL=index.2298a1cd.js.map
--- dist/index.3f07cc49.js
+++
@@ -1,2 +0,0 @@
-const s="Start test. (TS support)";console.log(s);
-//# sourceMappingURL=index.3f07cc49.js.map
